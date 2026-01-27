3309a5ebd24799eb45681a6055565634dfb7f84c
Starlight Deploy <deploy@starlight-gallery.com>
2026-01-27 22:13:00 +0200

    fix(exceptions): make previous parameter nullable for PHP 8.4

--- codeengage-backend/app/Exceptions/ApiException.php
+++ codeengage-backend/app/Exceptions/ApiException.php
@@ -7,7 +7,7 @@ class ApiException extends \Exception
     protected $statusCode;
     protected $errorData;
 
-    public function __construct(string $message, int $statusCode = 400, array $errorData = [], \Throwable $previous = null)
+    public function __construct(string $message, int $statusCode = 400, array $errorData = [], ?\Throwable $previous = null)
     {
         parent::__construct($message, 0, $previous);
         $this->statusCode = $statusCode;
